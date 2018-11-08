0f03e13d9919c1cd73bfc8234b63b4080dd04832
Dr Alun Moon <alun.moon@northumbria.ac.uk>
2018-11-08 20:53:25 +0000

    accelerometer and lcd working

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <mbed.h>
 #include <C12832.h>
 #include <LM75B.h>
+#include <MMA7660.h>
+
+#include <math.h>
 
 DigitalOut led1(LED1, 1);
 DigitalOut led2(LED2, 1);
@@ -12,7 +15,9 @@ DigitalOut led3(LED3, 1);
 
 C12832 lcd(D11, D13, D12, D7, D10);
 
-//LM75B sensor(D14,D15);
+LM75B sensor(D14,D15);
+
+MMA7660 MMA(D14,D15);
 
 void led2_thread()
 {
@@ -31,23 +36,33 @@ void led3_thread()
 }
 
 float T;
-float val;
+float val,x,y,z, g,gmax=0,gmin=100;
 void prt(void)
 {
 	while (true) {
 		lcd.locate(0, 0);
-		lcd.printf("T %.3f°C", val);
+		lcd.printf("T %.3f°C\n", val);
+		lcd.printf("a (%.2f,%.2f,%.2f) |%.2f|\n",x,y,z, g);
+		lcd.printf("[%.2f , %.2f]", gmin, gmax);
 		wait(1);
 	}
 }
 
-/*
+
 void pots(void) {
 	while(true){
 		val = (float)sensor.read();
+		x = MMA.x();
+		y = MMA.y();
+		z = MMA.z();
+
+		g = sqrt(x*x+y*y+z*z); 
+		gmax = (g>gmax)?g:gmax;
+		gmin = (g<gmin)?g:gmin;
+		wait(0.2);
 	}
 }
-*/
+
 
 
 void rgb(void)
@@ -59,8 +74,10 @@ void rgb(void)
 	PwmOut r(D5);
 	float n=0.1;
 	while (1) {
-		r=0.9;
-		wait(0.2);
+		r= n;
+		n-=0.01;
+		if (n<0.0) n=1.0;
+		wait(0.1);
 	}
 }
 
@@ -68,17 +85,16 @@ void rgb(void)
 //EventQueue q;
 
 Thread thread2;
-//Thread thread3;
-//Thread thread4;
-//Thread thread5;
+Thread thread3;
+Thread thread4;
+Thread thread5;
 Thread thread6;
 
 int main()
 {
-	int n;
 	thread6.start(rgb);
-	//thread5.start(pots);
-	//thread4.start(prt);
+	thread5.start(pots);
+	thread4.start(prt);
 	thread2.start(led2_thread);
 	//thread3.start(led3_thread);
 	while (true) {
